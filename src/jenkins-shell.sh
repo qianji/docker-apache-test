3a638b76a5da08d2ad72cdc94bc95ea12453a1e2
qianji <qianji.zheng@gmail.com>
2016-03-07 11:57:16 -0600

    Update jenkins-shell.sh

--- src/jenkins-shell.sh
+++ src/jenkins-shell.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-SERVICE_NAME="flask-signup-service"
+PROJECT_NAME="docker-shiny"
+SERVICE_NAME=$PROJECT_NAME"-service"
 IMAGE_VERSION="v_"${BUILD_NUMBER}
-TASK_FAMILY="flask-signup"
-
+TASK_FAMILY=$PROJECT_NAME
 # Create a new task definition for this build
-sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" flask-signup.json > flask-signup-v_${BUILD_NUMBER}.json
-aws ecs register-task-definition --family flask-signup --cli-input-json file://flask-signup-v_${BUILD_NUMBER}.json
+sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" $PROJECT_NAME.json > $PROJECT_NAME-v_${BUILD_NUMBER}.json
+aws ecs register-task-definition --family $PROJECT_NAME --cli-input-json file://$PROJECT_NAME-v_${BUILD_NUMBER}.json
 
 # Update the service with the new task definition and desired count
-TASK_REVISION=`aws ecs describe-task-definition --task-definition flask-signup | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
+TASK_REVISION=`aws ecs describe-task-definition --task-definition $PROJECT_NAME | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
 
 
 aws ecs update-service --cluster default --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count 1
