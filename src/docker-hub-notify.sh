50959887447a7c713b2deba9110ab427dd9fcdb2
qianji <qianji.zheng@gmail.com>
2016-03-08 17:06:22 -0600

    Update docker-hub-notify.sh

--- src/docker-hub-notify.sh
+++ src/docker-hub-notify.sh
@@ -2,7 +2,7 @@
 DOCKER_HUB_REPO_NAME="qianjizheng/docker-apache-test"
 docker ps -a | awk '{ print $1,$2 }' | grep $DOCKER_HUB_REPO_NAME | awk '{print $1 }' | xargs -I {} docker stop {}
 docker ps -a | awk '{ print $1,$2 }' | grep $DOCKER_HUB_REPO_NAME | awk '{print $1 }' | xargs -I {} docker rm {}
-aws ecs update-service --cluster default --service ${SERVICE_NAME} --task-definition --desired-count 0
+aws ecs update-service --cluster default --service ${SERVICE_NAME} --desired-count 0
 docker pull $DOCKER_HUB_REPO_NAME
 #docker run -d -p 81:80 $DOCKER_HUB_REPO_NAME
 #exit
