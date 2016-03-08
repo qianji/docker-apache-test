a17476729f26bcf51630ce310ca3f86f597ab267
qianji <qianji.zheng@gmail.com>
2016-03-08 00:04:24 -0600

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,14 +1,11 @@
-sudo yum install -y wget nginx git java vim
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
-sudo rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum install -y wget git java vim
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 sudo yum install -y jenkins
 sudo usermod -a -G docker jenkins
-sudo service docker start
+#sudo service docker start
 sudo service jenkins start
-sudo service nginx start
-sudo chkconfig docker on
 sudo chkconfig jenkins on
-sudo chkconfig nginx on
 sudo curl -O https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 sudo /usr/local/bin/pip install awscli
