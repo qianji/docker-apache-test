040e93a5bc756aba4f5c46d8130dd4d08ebcde87
qianji <qianji.zheng@gmail.com>
2016-03-08 20:13:27 -0600

    Create configure-docker-jenkins.sh

---
+++ src/configure-docker-jenkins.sh
@@ -0,0 +1,8 @@
+# We’ll be using Jenkins to build our Docker images, so we need to add the jenkins user to the docker group.
+sudo usermod -a -G docker jenkins
+
+# start docker, jenkins and make sure they will be running after a reboot
+sudo service jenkins start
+sudo chkconfig jenkins on
+sudo service docker start
+sudo chkconfig docker on
