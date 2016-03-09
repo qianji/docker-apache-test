1bd1fafa22964ff4794acaf39531ff2d56be29b6
qianji <qianji.zheng@gmail.com>
2016-03-08 20:12:50 -0600

    Update install-jenkins-ubuntu.sh

--- src/install-jenkins-ubuntu.sh
+++ src/install-jenkins-ubuntu.sh
@@ -3,9 +3,3 @@ wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install -y jenkins
-# We’ll be using Jenkins to build our Docker images, so we need to add the jenkins user to the docker group.
-sudo usermod -a -G docker jenkins
-
-# start docker, jenkins and make sure they will be running after a reboot
-sudo service jenkins start
-sudo chkconfig jenkins on
